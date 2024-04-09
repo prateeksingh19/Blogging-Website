a21c0a21d9a9021ad62057a42020d42e0b5addb2
Prateek Singh <prateekvns99@gmail.com>
2024-04-09 16:30:28 +0530

    init publish page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,8 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/signup";
 import { Signin } from "./pages/Signin";
 import { Blogs } from "./pages/Blogs";
-import "./App.css";
 import { Blog } from "./pages/Blog";
+import { Publish } from "./pages/Publish";
+import "./App.css";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/signin" element={<Signin />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/publish" element={<Publish />} />
         </Routes>
       </BrowserRouter>
     </>
--- FrontEnd/src/components/Appbar.tsx
+++ FrontEnd/src/components/Appbar.tsx
@@ -4,14 +4,24 @@ import { Link } from "react-router-dom";
 
 export function Appbar() {
   return (
-    <div className="border-b flex justify-between px-10 py-5">
-      <Link to={"/blogs"}>
-        <div className="font-bold flex flex-col justify-center text-xl cursor-pointer">
-          Medium
-        </div>
+    <div className="border-b flex justify-between px-10 py-4">
+      <Link
+        to={"/blogs"}
+        className="font-bold text-xl flex flex-col justify-center cursor-pointer"
+      >
+        Medium
       </Link>
       <div>
-        <Avatar size={"big"} name={"Prateek"} />
+        <Link to={`/publish`}>
+          <button
+            type="button"
+            className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 "
+          >
+            New
+          </button>
+        </Link>
+
+        <Avatar size={"big"} name="Prateek" />
       </div>
     </div>
   );
